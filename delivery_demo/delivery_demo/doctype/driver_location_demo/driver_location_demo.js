9918ed78a29dd576243cb53df24ea325d59b102a
jabdeen <power@DESKTOP-KD5S1TJ>
2023-10-25 01:34:09 +0300

    Changes to be committed: 	modified:   delivery_demo/__pycache__/tasks.cpython-310.pyc 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/__init__.py 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/__pycache__/__init__.cpython-310.pyc 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/__pycache__/driver_location_demo.cpython-310.pyc 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/driver_location_demo.js 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/driver_location_demo.json 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/driver_location_demo.py 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/templates/driver_location_demo.html 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/templates/driver_location_demo_row.html 	deleted:    delivery_demo/delivery_demo/doctype/driver_location_demo/test_driver_location_demo.py

--- delivery_demo/delivery_demo/doctype/driver_location_demo/driver_location_demo.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, jibreel and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("driver-location-demo", {
-// 	refresh(frm) {
-
-// 	},
-// });
