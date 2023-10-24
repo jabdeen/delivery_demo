84b0e23d518d1f5e1757912e64a1a5efe5c0cf89
jabdeen <power@DESKTOP-KD5S1TJ>
2023-10-25 01:43:30 +0300

    Changes to be committed: 	deleted:    delivery_demo/delivery_demo/doctype/driver_hired/__init__.py 	deleted:    delivery_demo/delivery_demo/doctype/driver_hired/driver_hired.js 	deleted:    delivery_demo/delivery_demo/doctype/driver_hired/driver_hired.json 	deleted:    delivery_demo/delivery_demo/doctype/driver_hired/driver_hired.py 	deleted:    delivery_demo/delivery_demo/doctype/driver_hired/test_driver_hired.py

--- delivery_demo/delivery_demo/doctype/driver_hired/driver_hired.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, jibreel and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("driver_hired", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- delivery_demo/delivery_demo/doctype/driver_hired/test_driver_hired.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, jibreel and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class Testdriver_hired(FrappeTestCase):
-	pass
